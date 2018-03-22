355f10345071e9df41a7bf6eab0c1f95c2317ab3
John Rodriguez <jcr2125@gmail.com>
2018-03-22 23:29:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ function happyHolidays () {
 }
 
 function happyHolidaysTo (name) {
-  return `Happy holidays, ${}`
+  return `Happy holidays, ${name}!`;
+}
+
+function happyHolidayTo (holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown (holiday, numDays) {
+  return `It's ${numDays} days until ${holiday}!`;
 }
